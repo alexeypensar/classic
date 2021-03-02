854ad27235803d0bb4b06849739a077533203168
alexeypensar <alexey@pensar.ru>
2021-03-02 12:38:36 +0700

    calculator is really working

--- js/script.js
+++ js/script.js
@@ -1,28 +1,7 @@
 // calc
 
-//disabled - gray
-
-
-const calculator = function() {
-    let prices = {
-        pvhClassic: 430,
-        pvhPremium: 450,
-        pvhEvolution: 530,
-        pvhPongs: 700,
-        pvhTeqtum: 750,
-        tkanDPremium: 990,
-        tkanDClipso: 2900,
-        tkanDCerutti: 2990,
-        pvhGold: 700,
-        corner: 200,
-        light: 250,
-        light2: 400
-    };
-
+let changingSpans = function() {
     let plusMinusArray = Array.from(document.getElementsByClassName('calculator__fieldset'));
-
-
-
 
     let calculating = function(fieldset, amountCounter) {
         let minus = fieldset.querySelector('.calculator__minus');
@@ -56,20 +35,68 @@ const calculator = function() {
     calculating(plusMinusArray[1], 4);
     calculating(plusMinusArray[2], 0);
     calculating(plusMinusArray[3], 0);
+};
+
+changingSpans();
+
+
+const calculator = function() {
 
 
     let calcResult = document.querySelector('.calculator__result');
 
     let materials = Array.from(document.getElementsByClassName('calculator__input'));
 
-    materials.addEventListener('change', function(el) {
-        let startPrice = 0;
-        if (el.value === prices.any)
+
+    let cornerPrice = 200;
+    let lightPrice = 250;
+    let light2Price = 400;
+
+    let square = +document.getElementById('calculatorSquareCount').textContent;
+    let corners = +document.getElementById('calculatorCornersCount').textContent;
+    let light = +document.getElementById('calculatorLightsCount').textContent;
+    let light2 = +document.getElementById('calculatorLights2Count').textContent;
+
+
+    let materialPrice = 0;
+
+    materials.find(function(el) {
+        if (el.checked === true) {
+            return materialPrice = el.value;
+        }
     });
 
 
+    calcResult.innerText = (materialPrice * square) + ((corners - 4) * cornerPrice) + (lightPrice * light) + (light2Price * light2);
 
 
 };
 
-calculator();
+let calculatorInteractiveInputs = Array.from(document.getElementsByClassName('calculator__input'));
+calculatorInteractiveInputs.forEach(function(el) {
+    el.addEventListener('change', function() {
+        calculator();
+    });
+});
+
+let calculatorInteractiveSpans = Array.from(document.getElementsByClassName('calculator__number'));
+
+calculatorInteractiveSpans.forEach(function(el) {
+
+    const config = {
+        attributes: true,
+        childList: true,
+        subtree: true
+    };
+
+    const callback = function(mutationsList, observer) {
+        for(const mutation of mutationsList) {
+            calculator();
+        }
+    };
+
+    const observer = new MutationObserver(callback);
+
+    observer.observe(el, config);
+});
+
